4195fbafeb42fe703255b36b6e2c2e43fb2938c0
Shiva <Shiva@192.168.2.13>
2017-09-09 13:23:35 -0400

    Blank Spring Boot Project

---
+++ DDEBBIEDeliveryAPI/src/main/java/com/ddebbie/delivery/DdebbieDeliveryApiApplication.java
@@ -0,0 +1,12 @@
+package com.ddebbie.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdebbieDeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdebbieDeliveryApiApplication.class, args);
+	}
+}
